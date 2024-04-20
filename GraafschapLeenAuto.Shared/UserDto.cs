72f7f2671329f9e412f384d074b8423e09620000
Lucas <l.kuster@covadis.nl>
2024-04-20 11:53:06 +0200

    les1

---
+++ GraafschapLeenAuto.Shared/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GraafschapLeenAuto.Shared
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ src/GraafschapLeenAuto.APi/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+using GraafschapLeenAuto.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraafschapLeenAuto.APi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        public static List<User> Users = new List<User>
+        {
+            new User
+            {
+                Name = "Name",
+                Email = "Mail",
+                Password = "Secret password"
+            }
+        };
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "Users")]
+        public IActionResult Get()
+        {
+            var usersDtos = Users.Select(user => new UserDto
+            {
+                Name = user.Name,
+                Email = user.Email
+            });
+
+            return Ok(usersDtos);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            Users.Add(user);
+            return Ok();
+        }
+    }
+}
+
+
+public class User
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
